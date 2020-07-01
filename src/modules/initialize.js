bf1c3ea5912612884249329057c6a2a76a653249
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-07-01 16:21:03 +0500

    added some functionality

---
+++ src/modules/initialize.js
@@ -0,0 +1,19 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getCoords, setDefaultCoords);
+  } else {
+    console.log('No Location Data');
+  }
+}
+
+function getCoords(position) {
+  localStorage.clear();
+  localStorage.setItem('latitude', position.coords.latitude);
+  localStorage.setItem('longitude', position.coords.longitude);
+}
+
+function setDefaultCoords() {
+  localStorage.clear();
+  localStorage.setItem('latitude', 43.653);
+  localStorage.setItem('longitude', 79.383);
+}
