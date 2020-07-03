2ab6be7669606cc849ac9987148f9226a71c2f75
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-07-03 19:59:58 +0500

    more refactoring

--- src/modules/initialize.js
+++ src/modules/initialize.js
@@ -5,27 +5,18 @@ const initialize = function init () {
     localStorage.setItem('latitude', position.coords.latitude);
     localStorage.setItem('longitude', position.coords.longitude);
     showDefault();
-    console.log('Get Coords');
-    console.log(localStorage);
   }
   
   const setDefaultCoords = () => {
     // Sets default location to Toronto if user denies permission to use geolocation
-    localStorage.setItem('latitude', 43.651);
-    localStorage.setItem('longitude', -79.347);
+    localStorage.setItem('latitude', 43.6548);
+    localStorage.setItem('longitude', -79.3883);
     showDefault();
-    console.log('Set Default Coords');
-    console.log(localStorage);
   }
 
 
   const getLocation = () => {
-    console.log('Running from initialize.getLocation');
-    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(getCoords, setDefaultCoords);
-    } else {
-      console.log('No Location Data');
-    }
   }
   
   return {
