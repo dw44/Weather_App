005cff348d4ce151b3f7d5c1a0f6c99ca617c66b
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-07-04 14:36:41 +0500

    functionality complete. styles / minor refactoring pending.

--- src/modules/display.js
+++ src/modules/display.js
@@ -7,24 +7,23 @@ const display = function() {
     if (unit === 1) {
       document.getElementById('current-temperature').innerText = `Current Temperature: ${printWithUnits(unitConversion().kelvinToCelsius(weather.main.temp).toFixed(1), 'C')}`;
       document.getElementById('feels-like').innerText = `Feels Like: ${printWithUnits(unitConversion().kelvinToCelsius(weather.main.feels_like).toFixed(1), 'C')}`;
-      document.getElementById('daily-max').innerText = `Maximum: ${printWithUnits(unitConversion().kelvinToCelsius(weather.main.feels_like).toFixed(1), 'C')}`;
-      document.getElementById('daily-min').innerText = `Minimum: ${printWithUnits(unitConversion().kelvinToCelsius(weather.main.feels_like).toFixed(1), 'C')}`;
+      document.getElementById('daily-max').innerText = `Maximum: ${printWithUnits(unitConversion().kelvinToCelsius(weather.main.temp_max).toFixed(1), 'C')}`;
+      document.getElementById('daily-min').innerText = `Minimum: ${printWithUnits(unitConversion().kelvinToCelsius(weather.main.temp_min).toFixed(1), 'C')}`;
     }
     else  if (unit === 2) {
       document.getElementById('current-temperature').innerText = `Current Temperature: ${printWithUnits(unitConversion().kelvinToFahrenheit(weather.main.temp).toFixed(1), 'F')}`;
       document.getElementById('feels-like').innerText = `Feels Like: ${printWithUnits(unitConversion().kelvinToFahrenheit(weather.main.feels_like).toFixed(1), 'F')}`;
-      document.getElementById('daily-max').innerText = `Maximum: ${printWithUnits(unitConversion().kelvinToFahrenheit(weather.main.feels_like).toFixed(1), 'F')}`;
-      document.getElementById('daily-min').innerText = `Minimum: ${printWithUnits(unitConversion().kelvinToFahrenheit(weather.main.feels_like).toFixed(1), 'F')}`;
+      document.getElementById('daily-max').innerText = `Maximum: ${printWithUnits(unitConversion().kelvinToFahrenheit(weather.main.temp_max).toFixed(1), 'F')}`;
+      document.getElementById('daily-min').innerText = `Minimum: ${printWithUnits(unitConversion().kelvinToFahrenheit(weather.main.temp_min).toFixed(1), 'F')}`;
     }
   }
 
   const displayAll = (weather, unit) => {
     document.getElementById('location').innerText = `${weather.name}, ${weather.sys.country}`;
     document.getElementById('humidity').innerText = `Humidity: ${weather.main.humidity}%`;
-    document.getElementById('wind-speed').innerText = `Wind Speed: ${weather.wind.speed} km/h`;
+    document.getElementById('wind-speed').innerText = `Wind Speed: ${(weather.wind.speed * 3.6).toFixed(1)} km/h`;
     document.getElementById('description-text').innerText = weather.weather[0].description;
-    console.log(weather.weather[0]);
-    console.log(weather.weather[0].description);
+    document.getElementById('description-image').src = `./assets/${weather.weather[0].icon}@2x.png`;
     displayWeather(weather, unit);
   }
 
